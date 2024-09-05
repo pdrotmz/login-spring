ac23ef80f8c4af64d50798eb0c8e75fa3b9c1d17
pdrotmz <pedrotomaz0210@gmail.com>
2024-09-05 14:18:38 -0300

    feat: add method findByEmail interface

--- src/main/java/de/pdrotmz/login_auth_api/repository/UserRepository.java
+++ src/main/java/de/pdrotmz/login_auth_api/repository/UserRepository.java
@@ -3,7 +3,10 @@ package de.pdrotmz.login_auth_api.repository;
 import de.pdrotmz.login_auth_api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
 }
