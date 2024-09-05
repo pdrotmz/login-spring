af642e709b5d55d6894b309e6399210843236523
pdrotmz <pedrotomaz0210@gmail.com>
2024-09-05 14:38:43 -0300

    feat: add CustomUserDetailsService class

---
+++ src/main/java/de/pdrotmz/login_auth_api/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package de.pdrotmz.login_auth_api.infra.security;
+
+import de.pdrotmz.login_auth_api.domain.user.User;
+import de.pdrotmz.login_auth_api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
