821cc601e526b84bab3440a4cb9d0b88faae8b68
pdrotmz <pedrotomaz0210@gmail.com>
2024-09-05 11:59:57 -0300

    initial commit

---
+++ src/test/java/de/pdrotmz/login_auth_api/LoginAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.pdrotmz.login_auth_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
