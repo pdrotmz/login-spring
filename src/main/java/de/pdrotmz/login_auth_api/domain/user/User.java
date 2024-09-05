d37c3b3aef4f03947057b8db03bebf2a4cb41fca
pdrotmz <pedrotomaz0210@gmail.com>
2024-09-05 12:56:26 -0300

    feat: add User model

---
+++ src/main/java/de/pdrotmz/login_auth_api/domain/user/User.java
@@ -0,0 +1,27 @@
+package de.pdrotmz.login_auth_api.domain.user;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private String email;
+    private String password;
+}
